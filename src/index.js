f051a241ee2f25cae1bea9375d80c2a279af8d62
deswinta343 <deswinta343@gmail.com>
2024-05-11 10:13:47 +0700

    ref-ui-II

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { render } from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 // Soft UI Dashboard React Context Provider
 import { SoftUIControllerProvider } from "context";
 
-createRoot(document.getElementById("root")).render(
+render(
   <HashRouter>
     <SoftUIControllerProvider>
       <App />
     </SoftUIControllerProvider>
-  </HashRouter>
+  </HashRouter>,
+  document.getElementById("root")
 );
